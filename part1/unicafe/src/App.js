ac0a015f1521a2e8d5e0e54a81f4207e6a101cc7
jiani <1285092865@qq.com>
2023-03-14 10:58:41 +0100

    unicafe step1-step5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const StatisticsLine = ({ text, value }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p>
+      {text} {value}
+    </p>
+  );
+};
+
+// a proper place to define a component
+const Statistics = ({ good, bad, neutral }) => {
+  const all = good + neutral + bad;
+  const average = (good * 1 + bad * -1) / all;
+  const positive = (good / all) * 100 + "%";
+
+  if (!good && !bad && !neutral) {
+    return <p>No feedback given</p>;
+  }
+  return (
+    <>
+      <StatisticsLine text="good" value={good}></StatisticsLine>
+      <StatisticsLine text="neutral" value={neutral}></StatisticsLine>
+      <StatisticsLine text="bad" value={bad}></StatisticsLine>
+      <StatisticsLine text="all" value={all}></StatisticsLine>
+      <StatisticsLine text="average" value={average}></StatisticsLine>
+      <StatisticsLine text="positive" value={positive}></StatisticsLine>
+    </>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  function handleClickGood() {
+    setGood(good + 1);
+  }
+
+  function handleClickNeutral() {
+    setNeutral(neutral + 1);
+  }
+
+  function handleClickBad() {
+    setBad(bad + 1);
+  }
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button handleClick={handleClickGood} text="good" />
+      <Button handleClick={handleClickNeutral} text="neutral" />
+      <Button handleClick={handleClickBad} text="bad" />
+      <h2>statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </div>
   );
-}
+};
 
 export default App;
